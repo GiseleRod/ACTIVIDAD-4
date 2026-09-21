1403a9f35c14abfd1a23361440f879ebb73eb0b9
FacundoMi <famiranda6@alumnos.unsada.edu.ar>
2026-09-20 21:25:16 -0300

    Implement main method to analyze array

--- src/main/java/grupo1/ejercicio1/Main.java
+++ src/main/java/grupo1/ejercicio1/Main.java
@@ -1,1 +1,19 @@
 
+package ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        MiArray miArray = new MiArray(1000);
+        miArray.llenarArray();
+        miArray.imprimirArray();
+
+        AnalizadorArray analizador = new AnalizadorArray(miArray.getLista());
+
+        int[] maximo = analizador.buscarMaximo();
+        int[] minimo = analizador.buscarMinimo();
+
+        System.out.println("\nEl número más alto: " + maximo[0] + " en la posición: " + maximo[1]);
+        System.out.println("El número más bajo: " + minimo[0] + " en la posición: " + minimo[1]);
+    }
+}
+
