0c675d993773eb533744c80cd7f440e69bf735cc
FacundoMi <famiranda6@alumnos.unsada.edu.ar>
2026-09-20 21:26:43 -0300

    Add MiArray class for array manipulation

---
+++ src/main/java/grupo1/ejercicio1/MiArray.java
@@ -0,0 +1,32 @@
+
+package ejercicio1;
+
+import java.util.Random;
+
+public class MiArray {
+    private int[] lista;
+    private Random random;
+
+    public MiArray(int tamaño) {
+        lista = new int[tamaño];
+        random = new Random();
+    }
+
+    public void llenarArray() {
+        for (int i = 0; i < lista.length; i++) {
+            lista[i] = random.nextInt(1000) + 1;
+        }
+    }
+
+    public void imprimirArray() {
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println("El número: " + lista[i] + " está en la posición: " + i);
+        }
+    }
+
+    // Getter para acceder al array desde otras clases
+    public int[] getLista() {
+        return lista;
+    }
+}
+
